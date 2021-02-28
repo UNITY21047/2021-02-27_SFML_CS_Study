b69e41d232f80544b23702c0c46c276c88032b7f
Gary Smith <unity21047@gmail.com>
2021-02-27 22:21:01 -0600

    Minor Upadte:
    Added memo for project.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2021_02_27_SFML_CS_Study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2021-02-27_SFML_CS_Study
+
+This is a study of the setup for SFML-CS.
